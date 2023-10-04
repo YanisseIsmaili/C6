a3a4adc3ffb633441ab41acafa1c0a6ae7a72f43
FORUM <yanisse.ismaili@ynov.com>
2023-10-04 16:37:28 +0200

    hello

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class HelloWorld_Exercice
+    {
+        public static string HelloWorld()
+        {
+            string hello = "Hello World !"
+            return hello
+        }
+    }
+}
