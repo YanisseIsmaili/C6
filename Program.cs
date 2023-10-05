ab820d1e1b6023142d9faf0320688048dd7931a9
FORUM <yanisse.ismaili@ynov.com>
2023-10-05 15:08:36 +0200

    reverse

--- Program.cs
+++ Program.cs
@@ -1,83 +1,14 @@
-﻿/*using System;
+﻿using System;
 using CSharpDiscovery.Quest01;
 
 namespace TestCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine(HelloWorld_Exercice.HelloWorld());
+            var inputTab = new int[] { 1, 32, 7, 33 };
+            Console.WriteLine("{0}", string.Join(" ", DoubleArrayForEvenInteger_Exercice.DoubleArrayForEvenInteger(inputTab)));
         }
     }
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////:/
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-using System;
-using CSharpDiscovery.Quest01;
-
-namespace TestCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(AddTwoIntegers_Exercice.AddTwoIntegers(1, 2));
-            Console.WriteLine(AddTwoIntegers_Exercice.AddTwoIntegers(10, 5));
-        }
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////:/
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-using System;
-using CSharpDiscovery.Quest01;
-
-namespace TestCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(IsOdd_Exercice.IsOdd(2));
-            Console.WriteLine(IsOdd_Exercice.IsOdd(11));
-            Console.WriteLine(IsOdd_Exercice.IsOdd(30));
-        }
-    }
-}
-
-
-using System;
-using CSharpDiscovery.Quest01;
-
-namespace TestCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(CSharpDiscovery.Quest01.SetPiValue_Exercice.SetPiValue(0));
-        }
-    }
-}
-
-using System;
-using CSharpDiscovery.Quest01;
-
-namespace TestCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(SpellItBackwards_Exercice.SpellItBackward("helloworld"));
-        }
-    }
-}
-*/
-
