435264081f00ab81f7fdabdd3cf6ffc8733701ec
FORUM <yanisse.ismaili@ynov.com>
2023-10-05 15:22:44 +0200

    reverse

--- Program.cs
+++ Program.cs
@@ -1,14 +1,99 @@
-﻿using System;
+﻿/*using System;
 using CSharpDiscovery.Quest01;
 
 namespace TestCSharp
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            var inputTab = new int[] { 1, 32, 7, 33 };
-            Console.WriteLine("{0}", string.Join(" ", DoubleArrayForEvenInteger_Exercice.DoubleArrayForEvenInteger(inputTab)));
+            Console.WriteLine(HelloWorld_Exercice.HelloWorld());
         }
     }
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////:/
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddTwoIntegers_Exercice.AddTwoIntegers(1, 2));
+            Console.WriteLine(AddTwoIntegers_Exercice.AddTwoIntegers(10, 5));
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////:/
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsOdd_Exercice.IsOdd(2));
+            Console.WriteLine(IsOdd_Exercice.IsOdd(11));
+            Console.WriteLine(IsOdd_Exercice.IsOdd(30));
+        }
+    }
+}
+
+
+using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CSharpDiscovery.Quest01.SetPiValue_Exercice.SetPiValue(0));
+        }
+    }
+}
+
+using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SpellItBackwards_Exercice.SpellItBackward("helloworld"));
+        }
+    }
+}
+*/
+
+
+using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] str)
+        {
+            Console.WriteLine(GetStringSize_Exercice.GetStringSize("helloworld"));
+        }
+    }
+}
+
+
---
+++ dzd.cs
@@ -0,0 +1,13 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class GetStringSize_Exercice
+    {
+        public static int GetStringSize(string str)
+        {
+           int stringLength = str.Length;
+           return stringLength;
+        }
+    }
+}
+
+
