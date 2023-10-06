759ceb285d055acbb721b5f73fdce3a425f2477c
FORUM <yanisse.ismaili@ynov.com>
2023-10-06 21:39:49 +0200

    point Of Interest

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -6,10 +6,11 @@ namespace CSharpDiscovery.Quest03
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty; // Initialize with an empty string
 
         public static string GoogleMapsUrlTemplate => "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
+        // Constructor with default values
         public PointOfInterest()
         {
             Name = "Bordeaux Ynov Campus";
@@ -17,6 +18,7 @@ namespace CSharpDiscovery.Quest03
             Longitude = -0.5663056;
         }
 
+        // Parameterized constructor
         public PointOfInterest(string name, double latitude, double longitude)
         {
             Name = name;
@@ -24,6 +26,7 @@ namespace CSharpDiscovery.Quest03
             Longitude = longitude;
         }
 
+        // Method to generate Google Maps URL
         public string GetGoogleMapsUrl()
         {
             return string.Format(GoogleMapsUrlTemplate, Uri.EscapeDataString(Name), Latitude, Longitude);
