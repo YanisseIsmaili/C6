7812bf541ef28e115e28eccf3491bc1405946105
FORUM <yanisse.ismaili@ynov.com>
2023-10-06 21:35:52 +0200

    point Of Interest

--- PointOfInterest2.cs
+++ PointOfInterest2.cs
@@ -2,31 +2,25 @@ using System;
 
 namespace CSharpDiscovery.Quest03
 {
-    public class PointOfInterest
+    public class PointOfInterest2
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public string Name { get; set; }
-
         public static string GoogleMapsUrlTemplate => "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
-        public PointOfInterest()
-        {
+        public PointOfInterest2()
+            {
             Name = "Bordeaux Ynov Campus";
             Latitude = 44.854186;
             Longitude = -0.5663056;
-        }
+            }
 
-        public PointOfInterest(string name, double latitude, double longitude)
-        {
+        public PointOfInterest2(string name, double latitude, double longitude)
+            {
             Name = name;
             Latitude = latitude;
             Longitude = longitude;
-        }
-        
-        public string GetGoogleMapsUrl()
-        {
-            return string.Format(GoogleMapsUrlTemplate, Uri.EscapeDataString(Name), Latitude, Longitude);
-        }
+            }
     }
 }
--- Program.cs
+++ Program.cs
@@ -338,9 +338,6 @@ namespace TestCSharp
         }
     }
 }
-*/
-
-
 
 using System;
 using CSharpDiscovery.Quest03;
@@ -360,6 +357,27 @@ namespace TestCSharp
             Console.WriteLine(ParisCampus.Name);
             Console.WriteLine(ParisCampus.Latitude);
             Console.WriteLine(ParisCampus.Longitude);
+        }
+    }
+}
+*/
+
+
+
+using System;
+using CSharpDiscovery.Quest03;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] str)
+        {
+            var DefaultPoint = new PointOfInterest();
+            Console.WriteLine(DefaultPoint.GetGoogleMapsUrl());
+
+            var ParisCampus = new PointOfInterest("Paris Ynov Campus", 48.9016552, 2.2079985);
+            Console.WriteLine(ParisCampus.GetGoogleMapsUrl());
         }
     }
 }
