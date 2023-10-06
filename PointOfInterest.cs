fe8e964f5df7c007a7eef31e05a26c884330cb9c
FORUM <yanisse.ismaili@ynov.com>
2023-10-06 21:43:33 +0200

    point Of Interest

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -10,26 +10,23 @@ namespace CSharpDiscovery.Quest03
 
         public static string GoogleMapsUrlTemplate => "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
-        // Constructor with default values
-        public PointOfInterest()
-        {
+    public PointOfInterest()
+            {
             Name = "Bordeaux Ynov Campus";
             Latitude = 44.854186;
             Longitude = -0.5663056;
-        }
+            }
 
-        // Parameterized constructor
-        public PointOfInterest(string name, double latitude, double longitude)
-        {
+    public PointOfInterest(string name, double latitude, double longitude)
+            {
             Name = name;
             Latitude = latitude;
             Longitude = longitude;
-        }
+            }
 
-        // Method to generate Google Maps URL
-        public string GetGoogleMapsUrl()
-        {
-            return string.Format(GoogleMapsUrlTemplate, Uri.EscapeDataString(Name), Latitude, Longitude);
-        }
+    public string GetGoogleMapsUrl()
+            {
+            return string.Format(GoogleMapsUrlTemplate, Uri.EscapeUriString(Name), Latitude, Longitude);
+            }
     }
 }
--- Program.cs
+++ Program.cs
@@ -360,10 +360,27 @@ namespace TestCSharp
         }
     }
 }
+
+
+
+using System;
+using CSharpDiscovery.Quest03;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] str)
+        {
+            var DefaultPoint = new PointOfInterest();
+            Console.WriteLine(DefaultPoint.GetGoogleMapsUrl());
+
+            var ParisCampus = new PointOfInterest("Paris Ynov Campus", 48.9016552, 2.2079985);
+            Console.WriteLine(ParisCampus.GetGoogleMapsUrl());
+        }
+    }
+}
 */
-
-
-
 using System;
 using CSharpDiscovery.Quest03;
 
