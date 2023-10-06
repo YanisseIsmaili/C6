98c88758d24d27f8d25746da103c4e0331cd0ad8
FORUM <yanisse.ismaili@ynov.com>
2023-10-06 15:47:20 +0200

    contains

--- Program.cs
+++ Program.cs
@@ -185,7 +185,7 @@ namespace TestCSharp
         }
     }
 }
-*/
+
 
 using System;
 using CSharpDiscovery.Quest02;
@@ -197,6 +197,24 @@ namespace TestCSharp
         static void Main(string[] args)
         {
             Console.WriteLine(DisplayDateWithoutTime_Exercice.DisplayDateWithoutTime(new DateTime(2021, 1, 14)));
+        }
+    }
+}
+*/
+
+using System;
+using CSharpDiscovery.Quest02;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = new DateTime(2021, 02, 01, 12, 0, 0, 0);
+            var end = new DateTime(2021, 02, 01, 18, 0, 0, 0);
+            Console.WriteLine(DifferenceInMinutes_Exercice.DifferenceInMinutes(start, end));
+
         }
     }
 }
