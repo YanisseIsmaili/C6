6ab4d30864dab63ef591baa8a48392354d1040c4
FORUM <yanisse.ismaili@ynov.com>
2023-10-06 21:26:07 +0200

    point Of Interest

--- Program.cs
+++ Program.cs
@@ -319,7 +319,7 @@ namespace TestCSharp
     }
 }
 
-*/
+
  using System;
 using CSharpDiscovery.Quest03;
 
@@ -338,7 +338,28 @@ namespace TestCSharp
         }
     }
 }
+*/
 
 
 
+using System;
+using CSharpDiscovery.Quest03;
 
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] str)
+        {
+            var DefaultPoint = new PointOfInterest();
+            Console.WriteLine(DefaultPoint.Name);
+            Console.WriteLine(DefaultPoint.Latitude);
+            Console.WriteLine(DefaultPoint.Longitude);
+
+            var ParisCampus = new PointOfInterest("Paris Ynov Campus", 48.9016552, 2.2079985);
+            Console.WriteLine(ParisCampus.Name);
+            Console.WriteLine(ParisCampus.Latitude);
+            Console.WriteLine(ParisCampus.Longitude);
+        }
+    }
+}
