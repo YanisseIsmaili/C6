ede14e2b613b08261367593bc6a8fefffadddb42
FORUM <yanisse.ismaili@ynov.com>
2023-10-06 16:49:00 +0200

    contains

---
+++ ContainsThisValue.cs
@@ -0,0 +1,22 @@
+using System.Linq; // Required for the Contains method
+
+namespace CSharpDiscovery.Quest02
+{
+    public class ContainsThisValue_Exercice
+    {
+        public static bool ContainsThisValue(int[] tab, int value)
+        {
+            if (tab == null)
+            {
+                return false;
+            }
+
+            if (tab.Contains(value))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -218,7 +218,7 @@ namespace TestCSharp
         }
     }
 }
-*/
+
 using System;
 using CSharpDiscovery.Quest02;
 
@@ -229,6 +229,24 @@ namespace TestCSharp
         static void Main(string[] args)
         {
             Console.WriteLine(FromStringToDateTime_Exercice.FromStringToDateTime("21/12/2021 18:33"));
+        }
+    }
+}
+*/
+
+using System;
+using CSharpDiscovery.Quest02;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tab = new int[5] { 99, 98, 92, 97, 95 };
+            var tab2 = new int[5] { 50, 54, 85, 47, 2 };
+            Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab, 97));
+            Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab2, 51));
         }
     }
 }
