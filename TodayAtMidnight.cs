ee994389726055daa40061cc082f4f8c2bba8d3e
FORUM <yanisse.ismaili@ynov.com>
2023-10-06 15:36:34 +0200

    contains

---
+++ TodayAtMidnight.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class TodayAtMidnight_Exercice
+    {
+        public static DateTime TodayAtMidnight()
+        {
+            DateTime date = DateTime.Today;
+            return date;
+        }
+    }
+}
+
+
+
+
+
